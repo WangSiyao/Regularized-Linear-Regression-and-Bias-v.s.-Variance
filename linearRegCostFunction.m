1d1a041c3aad12b1425adfa923a16b8eec3c996e
Wang Siyao <1521925222@qq.com>
2016-04-18 02:18:56 +0800

    finished

--- linearRegCostFunction.m
+++ linearRegCostFunction.m
@@ -19,9 +19,8 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
+J = sum((X * theta - y).^ 2) / 2 / m + sum(theta(2:end,:).^2) / 2 / m * lambda;
+grad = X' * (X * theta - y) / m + [0; theta(2:end,:)] * lambda / m;
 
 
 
